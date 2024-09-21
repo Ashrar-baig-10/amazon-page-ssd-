a217dda3a6f41349296b07a71ad25bfdbf1e4f3a
ashrar baig <ashrar.baigofficial@gmail.com>
2024-09-21 19:14:39 +0530

    make paymentsummary interactive

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -4,10 +4,8 @@ import { formatCurrency } from "../utils/money.js";
 // import{hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm,js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from "./paymentsummary.js";
 
-// const today=dayjs();
-// const deliveryDate=today.add(7, 'days');
-// console.log(deliveryDate.format('dddd, MMMM D'))
 
 export function renderOrderSummary(){
 
@@ -112,6 +110,8 @@ export function renderOrderSummary(){
                 removeFromCart(productId);
             const container= document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderPaymentSummary();
             });
         });
 
@@ -120,6 +120,7 @@ export function renderOrderSummary(){
                 const {productId,deliveryOptionId}=element.dataset;
                 updateDeliveryOption(productId,deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
 }
