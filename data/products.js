2e334afef76351435f0ccbdc0d7f9438cddfc7e7
ashrar baig <ashrar.baigofficial@gmail.com>
2024-10-02 19:04:27 +0530

    learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -53,6 +53,31 @@ class Clothing extends Product{
     }
 }
 
+// const date=new Date();
+// console.log(date)
+// console.log(date.toLocaleTimeString());
+
+/*const object2={
+  a:2,
+  b:this.a
+}*/
+
+
+/*function logThis(){
+  console.log(this);
+}
+
+logThis();
+logThis.call('hello');
+
+this
+const object3={
+  method:()=>{
+    console.log(this)
+  }
+}
+object3.method()
+*/
 
 
 export const products = [
