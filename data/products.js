fe0fe5878b4617f9047195c0d5a89c5115f4d480
ashrar baig <ashrar.baigofficial@gmail.com>
2024-10-02 19:05:15 +0530

    changed comments

--- data/products.js
+++ data/products.js
@@ -53,9 +53,10 @@ class Clothing extends Product{
     }
 }
 
-// const date=new Date();
-// console.log(date)
-// console.log(date.toLocaleTimeString());
+/* const date=new Date();
+console.log(date)
+console.log(date.toLocaleTimeString());
+*/
 
 /*const object2={
   a:2,
